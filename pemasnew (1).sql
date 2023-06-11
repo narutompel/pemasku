9050243124d7cfd794d2769b08e7d7bc9223c72b
narutompel <putujackson0@gmail.com>
2023-06-11 14:36:11 +0800

    Adi menambahkan database pemasku

---
+++ pemasnew (1).sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 08, 2023 at 03:02 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pemasnew`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masyarakat`
+--
+
+CREATE TABLE `masyarakat` (
+  `nik` varchar(16) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `telp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`nik`, `nama`, `email`, `username`, `password`, `telp`) VALUES
+('3511111605020112', 'Bayu Gede', 'bayugede@gmail.com', 'bayu12', 'bayu123', '0821212');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaduan`
+--
+
+CREATE TABLE `pengaduan` (
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_pengaduan` date NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `isi_laporan` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `status` enum('0','Proses','Selesai','Tolak') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
+(7, '2023-05-22', '3511111605020112', 'membuat aduan test', '', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_user` varchar(35) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `level` enum('Admin','Petugas','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `nama_user`, `email`, `username`, `password`, `telp`, `level`) VALUES
+(4, 'krisna megadana', 'putukrisnamegadan@gmail.com', 'krisna', 'krisna123', '081999271564', 'Admin'),
+(5, 'putu ', 'putu123@gmail.com', 'putu', 'putu123', '081999271584', 'Petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tanggapan`
+--
+
+CREATE TABLE `tanggapan` (
+  `id_tanggapan` int(11) NOT NULL,
+  `id_pengaduan` int(11) NOT NULL,
+  `tgl_tanggapan` date NOT NULL,
+  `tanggapan` text NOT NULL,
+  `id_petugas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `masyarakat`
+--
+ALTER TABLE `masyarakat`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- Indexes for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD PRIMARY KEY (`id_pengaduan`),
+  ADD KEY `pengaduan_ibfk_1` (`nik`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD PRIMARY KEY (`id_tanggapan`),
+  ADD KEY `tanggapan_ibfk_1` (`id_petugas`),
+  ADD KEY `id_pengaduan` (`id_pengaduan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `masyarakat` (`nik`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD CONSTRAINT `tanggapan_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tanggapan_ibfk_2` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
